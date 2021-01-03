5ce1a656d02935b70d71daa08af8cc3f762e3d4b
Ralph King Jr <ralph@kingralph.dev>
2021-01-02 20:42:18 -0500

    saving work

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,13 @@ import Layout from '../components/Layout'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import {MDXProvider} from '@mdx-js/react'
 
 library.add(fab, faEnvelope)
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <MDXProvider>
         <Component {...pageProps} />
-      </MDXProvider>
     </Layout>
   )
 }
