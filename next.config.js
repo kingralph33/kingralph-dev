5c28e84adb000e17bb9333a0a9a113cc27d2b892
Ralph King Jr <ralph@kingralph.dev>
2021-01-02 19:11:38 -0500

    saving work

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
+    extension: /\.(md|mdx)$/,
 })
 module.exports = withMDX({
-    pageExtensions: ["js", "jsx", "md", "mdx"],
+    // Pick up MDX files in the /pages/ directory
+    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
 })
