25141f0367f4e36823ab110aa7bfd0a2a2cfdb6f
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2022-03-18 11:39:01 -0400

    update GA

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,28 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-            <Head>
-            <link rel="icon" href="../public/images/ralph_king_memoji.png"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Cantarell:ital,wght@1,400;1,700&display=swap" rel="stylesheet"/>
-            {/* Global site tag (gtag.js) - Google Analytics */}
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}/>
-            <script
-            dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}'),
-                page_path: window.location.pathname,
-                });
-                `,
-            }}
-            />
+        <Head>
+          <link rel="shortcut icon" href="/public/images/ralph_king_memoji.png"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Cantarell:ital,wght@1,400;1,700&display=swap" rel="stylesheet"/>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
